a87824746f5b3c49d59fdcd438c226a4d6bea5e7
umarluqman <umarluqman.78@gmail.com>
2024-11-20 18:12:11 +0800

    revert back

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next"
 import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
 import { getBaseURL } from "@lib/util/env"
-import { Suspense } from "react"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -12,9 +11,7 @@ export const metadata: Metadata = {
 export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Nav />
-      </Suspense>
+      <Nav />
       {props.children}
       <Footer />
     </>
