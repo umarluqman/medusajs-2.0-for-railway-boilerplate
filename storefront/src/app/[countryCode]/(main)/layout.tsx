09a686e41d0cdf095d021534574ce4e2edd3c32a
umarluqman <umarluqman.78@gmail.com>
2024-11-20 17:32:48 +0800

    fix the build issue

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next"
 import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
 import { getBaseURL } from "@lib/util/env"
+import { Suspense } from "react"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -11,7 +12,9 @@ export const metadata: Metadata = {
 export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
-      <Nav />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Nav />
+      </Suspense>
       {props.children}
       <Footer />
     </>
